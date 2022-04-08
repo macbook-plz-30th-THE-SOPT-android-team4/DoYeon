63e69f61b3295d3ef9bddc7b37ae601c3afdb765
dddooo9 <dlaehdus99@naver.com>
2022-04-09 02:42:41 +0900

    [FEATURE] SignUp 회원가입 완료 버튼 클릭 이벤트 구현

--- app/src/main/java/com/doyeon/soptseminar/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/doyeon/soptseminar/ui/signup/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.doyeon.soptseminar.R
 import com.doyeon.soptseminar.databinding.ActivitySignUpBinding
 import com.doyeon.soptseminar.util.BaseViewUtil
+import com.doyeon.soptseminar.util.showShortToast
 
 class SignUpActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
@@ -14,5 +15,23 @@ class SignUpActivity :
     }
 
     override fun initView() {
+        setClickListener()
     }
+
+    private fun setClickListener() {
+        binding.abSignUpSignup.setOnClickListener {
+            if (isNameAndIdAndPwFieldFilled()) {
+                finish()
+            } else {
+                showShortToast(resources.getString(R.string.signup_fail_toast))
+            }
+        }
+    }
+
+    private fun isNameAndIdAndPwFieldFilled(): Boolean {
+        return binding.etSignUpName.text.toString().isNotEmpty()
+                && binding.etSignUpId.text.toString().isNotEmpty()
+                && binding.etSignUpPw.text.toString().isNotEmpty()
+    }
+
 }
